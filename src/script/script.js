db8928af0d23e8475763f531581903b70f5359a0
grmnga <anna_titomer@mail.ru>
2021-02-18 16:28:55 +0500

    fix percent for ship background

--- src/script/script.js
+++ src/script/script.js
@@ -13,7 +13,7 @@ var percent = scrollTop/scrollHeight;
 var color = {r:0,g:0,b:0,a:0};
 
 //tmp = Math.abs(bgColor1.a - bgColor2.a)*percent;
-tmp = 3*percent - 0.7;
+tmp = 3*percent - 0.5;
 color.a =  bgColor1.a > bgColor2.a ? bgColor1.a - tmp : bgColor1.a + tmp;
 color.a = color.a.toFixed(2)
 if (color.a > 0.99 || color.a < -0.5) {
