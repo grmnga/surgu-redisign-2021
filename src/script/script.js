5b0a3d3f6d971a2aca2b8e69daa344af4d69ba4b
grmnga <anna_titomer@mail.ru>
2021-03-12 10:25:43 +0500

    fix ship background opacity

--- src/script/script.js
+++ src/script/script.js
@@ -11,25 +11,28 @@ window.onload = function(){
     $('.category').on('click', categoryMenuButtonClick);
     $('.category-menu .categories button').on('click', categoryButtonClick)
 }
-window.addEventListener("scroll",function(){
-var scrollTop = window.pageYOffset || document.documentElement.scrollTop;//текущая позиция скролла
-var scrollHeight = innerHeight;
-var percent = scrollTop/scrollHeight;
-var color = {r:0,g:0,b:0,a:0};
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;//текущая позиция скролла
+    var scrollHeight = innerHeight;
+    var percent = scrollTop/scrollHeight;
+    var color = {r:0,g:0,b:0,a:0};
 
-//tmp = Math.abs(bgColor1.a - bgColor2.a)*percent;
-tmp = 3*percent - 0.5;
-color.a =  bgColor1.a > bgColor2.a ? bgColor1.a - tmp : bgColor1.a + tmp;
-color.a = color.a.toFixed(2)
-if (color.a > 0.99 || color.a < -0.5) {
-    return;
-}
-    
-document.getElementById("ship_color_background").style.opacity = color.a;
-document.getElementById("ship_text_background").style.opacity = color.a;
-document.getElementById("ship_bar").style.opacity = color.a;
-//    console.log(innerHeight*(1-percent)+160);
-document.getElementById("ship").style.bottom = innerHeight*(1-percent)+160;
+    //tmp = Math.abs(bgColor1.a - bgColor2.a)*percent;
+    tmp = 3*percent - 0.9;
+    color.a =  bgColor1.a > bgColor2.a ? bgColor1.a - tmp : bgColor1.a + tmp;
+    color.a = color.a.toFixed(2);
+    color.b = color.a;
+    if (color.a > 0.99) {
+        color.a = 1;
+    }
+    document.getElementById("ship_color_background").style.opacity = color.a;
+    document.getElementById("ship_text_background").style.opacity = color.a;
+    if (color.b > 0.99 || color.a < -0.5) {
+        return;
+    }
+    document.getElementById("ship_bar").style.opacity = color.a;
+    //    console.log(innerHeight*(1-percent)+160);
+    //document.getElementById("ship").style.bottom = innerHeight*(1-percent)+160;
 
 });
 
